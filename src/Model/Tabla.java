bbcc20ab7890e14ea911dc5c605d27b88674f937
SnowGRH <gergelyovinczeffy@gmail.com>
2024-02-12 14:07:23 +0100

    4,5

--- src/Model/Tabla.java
+++ src/Model/Tabla.java
@@ -1,27 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
+
+import java.util.Random;
 
 /**
  *
  * @author gerge
  */
 public class Tabla {
-    private char[][] T;
-    private char UresCella;
-    
 
-    public Tabla(char UresCella) {
-        this.T = new char[8][8];
+    private String[][] T;
+    private String UresCella;
+    private final Random rnd = new Random();
+
+    public Tabla(String UresCella) {
+        this.T = new String[8][8];
         this.UresCella = UresCella;
         for (int i = 0; i < T.length; i++) {
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < T.length; j++) {
                 this.T[i][j] = this.UresCella;
             }
         }
     }
-    
-    
+
+    public String Megjelenít() {
+        String txt = "";
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                txt += T[i][j];
+            }
+            txt += "\n";
+        }
+        return txt;
+    }
+
+    public void Elhelyez() {
+        String k = "K";
+        int kdb = 2;
+        for (int cz = 0; cz < kdb; cz++) {
+            int szi = rnd.nextInt(9);
+            int szj = rnd.nextInt(9);
+            for (int i = 0; i < T.length; i++) {
+                for (int j = 0; j < T.length; j++) {
+                    if (szi == i && szj == j && T[i][j] == UresCella) {
+                            T[i][j] = k;
+                    }
+                }
+            }
+        }
+    }
 }
